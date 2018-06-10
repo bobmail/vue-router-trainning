a813d9006ca72aba3b388ee0f6e28e2097631c85
Bob <bobmail@sina.com>
2018-06-10 12:43:00 -0700

    charpter01 finished

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
+    <div>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/hi">HiPage</router-link> |
+    </div>
     <router-view/>
   </div>
 </template>
---
+++ src/components/Hi.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h2>
+      {{msg}}
+    </h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Hi',
+  data () {
+    return {
+      msg: 'Bejour! welcome Hi1'
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
